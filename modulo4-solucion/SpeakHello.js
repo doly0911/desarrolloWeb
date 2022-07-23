11e53c75312ffe543256a7aa72d486b4d139ac00
Doly Jimenez <doly.jimenez11@gmail.com>
2022-07-23 00:04:12 -0500

    Se envuelve el sript SpeakHello.js dentro de un IIFE

--- modulo4-solucion/SpeakHello.js
+++ modulo4-solucion/SpeakHello.js
@@ -5,7 +5,8 @@
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-var helloSpeaker = speak();
+( function () {
+  var helloSpeaker = speak();
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
@@ -16,6 +17,8 @@ var speakWord = "Hello";
 function speak(name) {
   console.log(helloSpeaker + " " + name);
 }
+
+})();
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
